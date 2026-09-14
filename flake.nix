c8f4041b9af64d18a13fc95bc18a9b630ff8f529
codanael <43133220+codanael@users.noreply.github.com>
2026-09-14 08:51:05 +0000

    chore: Zitadel 4.17.3

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     zitadel-src = {
-      url = "github:zitadel/zitadel/v4.16.3";
+      url = "github:zitadel/zitadel/v4.17.3";
       flake = false;
     };
   };
